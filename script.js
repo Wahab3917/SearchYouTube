eae87b0b95578f99e410d0ef6a5cd1443b0f9ef2
Wahab3917 <63443048+Wahab3917@users.noreply.github.com>
2021-02-10 16:48:22 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Selecting Elements
+const preLoader = document.querySelector(".pre-loader");
 const searchForm = document.querySelector('#search');
 const searchInput = document.querySelector('#search-input');
 const videoContainer = document.querySelector('#video-container');
@@ -10,20 +11,28 @@ const YOUTUBE_API_KEY = "AIzaSyABsnIkGtmjmbQ2XXkl16AoyIhy40sYPKA";
 
 
 searchForm.addEventListener('submit', e => {
-
   e.preventDefault();
   const search = searchInput.value.replaceAll(' ', '%20');
 
   videoContainer.innerHTML = '';
 
+  // Pre Loader Start
+  preLoader.style.display = 'block';
+
+  displayResult(search);
+});
+
+
+const displayResult = async function(search) {
+  
   const url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${search}&key=${YOUTUBE_API_KEY}`;
   // console.log(url);
 
-
-  fetch(url)
+  try {
+    fetch(url)
     .then(response => response.json())
     .then(data => {
-      // console.log(data);
+    // console.log(data);
       
       data.items.forEach(item => {
         const videoId = item.id.videoId;
@@ -36,10 +45,14 @@ searchForm.addEventListener('submit', e => {
         `;
       });
 
-    })
-    .catch(err => {
-      alert(`Result not found, ${err.message}`);
-    })
+      // Pre Loader Finish
+      preLoader.style.display = 'none';
+
+    });
   
-});
+  } catch(err) {
+      alert(`Result not found, Try Again. ${err.message}`);
+  }
+
+};
 
